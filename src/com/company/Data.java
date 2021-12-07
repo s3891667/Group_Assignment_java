1c220de1964c484d9d169653966bae7b80afc36d
Owen <s3891667@rmit.edu.vn>
2021-12-08 04:18:23 +0700

    User area input for user + days range type 1.

--- src/com/company/Data.java
+++ src/com/company/Data.java
@@ -1,42 +1,23 @@
 package com.company;
-
-
 import java.util.ArrayList;
 
 public class Data extends Main{
-    protected String continent;
-    protected String time_range;
-    protected String country;
-
-    public Data (String continent, String time_range, String country){
-        this.continent = continent;
-        this.time_range = time_range;
-        this.country = country;
+    private final String area;
+    private  final int days;
+    public Data(String area, int days) {
+        this.area = area;
+        this.days = days;
     }
+
 
     public  void Data_division(){
-            ArrayList <String> data_list = new ArrayList<String>();
-            data_list.add(continent);
-            data_list.add(time_range);
-            data_list.add(country);
-            System.out.println(data_list);
-
-
 
     }
-
-
-
 
     public String continent (){
-        return continent;
+        return area;
     }
-    public String time_rage(){
-        return time_range;
-    }
-    public String country(){
-        return country;
-    }
+
 
 
 }
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,31 +1,109 @@
 package com.company;
-import java.util.Date;
-import java.text.SimpleDateFormat;
+
+import java.time.LocalDate;
+
+import java.time.format.DateTimeFormatter;
+
 import java.util.Scanner;
-
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.net.URL;
-import java.net.URLConnection;
 
-
+import static java.time.temporal.ChronoUnit.DAYS;
 
 public class Main {
-    public static void main(String[] args) throws IOException{
-            Scanner scanner = new Scanner(System.in);
-        System.out.println("please enter");
-            String user = scanner.nextLine();
-            String input_country = scanner.nextLine();
-            String input_continent = scanner.nextLine();
-            String input_time_range = scanner.nextLine();
-            if (user.equals("ok")){
-                Data inputdata = new Data(input_continent,input_time_range,input_country);
-                inputdata.Data_division();
+    public Main() {
+    }
+
+    public static void main(String[] args){
+        LocalDate init,end;
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Do you want to start the program (Y/N) ?");
+        char user =scanner.next().charAt(0);
+            if(user == 'Y'){
+                System.out.println("Welcome!");
+            }
+            else if (user =='N'){
+                System.out.println("Thank you for using our program!");
+
+            }
+            else {
+                System.out.println("Please enter again");
             }
 
+
+            System.out.println("Please choose 1 to input Continent, 2 to input Country ");
+        int user2 = scanner.nextInt();
+            if (user2 ==1){
+                System.out.println("Please input the Continent  "); }
+            else if(user2 == 2) {
+                System.out.println("Please input the country"); }
+            String area = scanner.next();
+
+
+
+        //Type1
+            init = init_date();
+        end = end_date();
+        int days = (int) DAYS.between(init, end);
+
+
+
+
+
+
+
+
+        Data data = new Data(area,days);
+        data.Data_division();
+
     }
+
+
+
+
+
+
+
+
+
+    public static LocalDate init_date(){
+        String startDate1;
+        LocalDate init;
+
+        Scanner sc = new Scanner(System.in);
+        DateTimeFormatter format = DateTimeFormatter.ofPattern("MM/dd/yyyy");
+
+        System.out.print("Start date: ");
+        startDate1 = sc.nextLine();
+        init = LocalDate.parse(startDate1, format);
+        return init;
+    }
+    public static LocalDate end_date() {
+        String endDate1;
+        LocalDate end;
+
+        Scanner sc = new Scanner(System.in);
+        DateTimeFormatter df = DateTimeFormatter.ofPattern("MM/dd/yyyy");
+
+        System.out.print("End date: ");
+        endDate1 = sc.nextLine();
+        end = LocalDate.parse(endDate1, df);
+
+        return end;
+    }
+
+    public static void Day_type(){
+
+    }
+
+
+    public static void Groups_type(){
+
+    }
+
+    public static void Nums_of_days_type(){
+
+    }
+
 
 
 }
