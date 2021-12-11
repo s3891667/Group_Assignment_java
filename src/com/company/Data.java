35d400a68aa5d547638946bb1384526ab19fe1cb
Owen <s3891667@rmit.edu.vn>
2021-12-12 00:40:28 +0700

    implemented!

--- src/com/company/Data.java
+++ src/com/company/Data.java
@@ -1,13 +1,112 @@
 package com.company;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
+import java.util.List;
 
 public class Data extends Main{
     private final String area;
-    private  final int days;
-    public Data(String area, int days) {
+    private final LocalDate init;
+    private final LocalDate end;
+    ArrayList <Integer > cases = new ArrayList<>();
+    ArrayList <Integer > deaths = new ArrayList<>();
+    ArrayList <Long> vaccinated = new ArrayList<>();
+
+    public Data(String area, LocalDate init, LocalDate end ) {
         this.area = area;
-        this.days = days;
+        this.init = init;
+        this.end = end;
     }
+
+
+    public String Quering_method(String area, LocalDate init, LocalDate end){
+        String address = "covid-data.csv";
+        String line ;
+        try {
+            BufferedReader br = new BufferedReader(new FileReader(address));
+            DateTimeFormatter formatter = DateTimeFormatter.ofPattern("M/d/yyyy");
+            //skip first line
+            br.readLine();
+            while ((line = br.readLine()) != null) {
+                String[] values = line.split(",");
+                LocalDate compare = LocalDate.parse(values[3],formatter);
+                //since the user is going to input continent or country so i will define those into same variable
+                if (values[2].equals(area) | values[1].equals(area)) {
+                    // Convert value at column 4 to Integer;
+                    int case_num = Integer.parseInt(values[4]);
+                    if (compare.isAfter(init.minusDays(1)) && compare.isBefore(end.plusDays(1))) {
+
+                        //if the cells in csv file is blank
+                        if (values[5] == null || values[5].equals("")) {
+                            values[5] = String.valueOf(0);
+                        }
+                        if (values[6] == null || values[6].equals("")) {
+                            values[6] = String.valueOf(0);
+                        }
+
+                        int death_num = Integer.parseInt(values[5]);
+                        long vaccinated_num = Long.parseLong(values[6]);
+                        cases.add(case_num);
+                        deaths.add(death_num);
+                        vaccinated.add(vaccinated_num); }
+                }
+                else {
+                    br.readLine();
+                }
+            }
+            System.out.println( "Infected cases : " +cases);
+            System.out.println("Death cases : " +  deaths);
+            System.out.println("Vaccinated : " +vaccinated);
+        }
+
+        catch (IOException e) {
+            e.printStackTrace(); }
+
+
+
+        return "done";
+    }
+
+
+
+
+
+    public Integer get_cases(){
+        int count= 0;
+        List<Integer> nums =new ArrayList<>(cases);
+        for (Integer num: nums  ){
+            count += num;
+        }
+
+        return count;
+    }
+
+
+    public Integer get_deaths(){
+        int count= 0;
+        List<Integer> nums =new ArrayList<>(deaths);
+        for (Integer num: nums  ){
+            count += num;
+        }
+
+        return count;
+    }
+
+
+    public Long get_vaccinated(){
+        long count= 0;
+        List<Long> nums =new ArrayList<>(vaccinated);
+        for (Long num: nums  ){
+            count += num;
+        }
+
+        return count;
+    }
+
+
 
 
     public  void Data_division(){
@@ -15,9 +114,17 @@ public class Data extends Main{
     }
 
     public String continent (){
+
         return area;
     }
 
 
+    public LocalDate getInit() {
+        return init;
+    }
+
+    public LocalDate getEnd() {
+        return end;
+    }
 
 }
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,19 +1,10 @@
 package com.company;
-import java.io.BufferedReader;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
 import java.time.LocalDate;
-
 import java.time.format.DateTimeFormatter;
-
 import java.util.*;
-import java.io.IOException;
-
-import static java.time.temporal.ChronoUnit.DAYS;
 
 public class Main {
     public static void main(String[] args){
-        int days = 0;
         LocalDate init = null, end = null;
         Scanner scanner = new Scanner(System.in);
         System.out.println("Do you want to start the program (Y/N) ?");
@@ -41,8 +32,6 @@ public class Main {
 
         //Area
         String area = scanner.next();
-
-
         System.out.println("Please choose the time rage type");
         System.out.println("Choose 1 for A pair of start date and end date (inclusive)");
         System.out.println("Choose 2 for A number of days or weeks from a particular date ");
@@ -51,21 +40,14 @@ public class Main {
 
 
 
-
-
-
-
-
-
         //Type1
         int user3 = scanner.nextInt();
-        if (user3 ==1) {
+        if (user3 == 1) {
             init = init_date();
             end = end_date();
-            days = (int) DAYS.between(init, end);
         }
 
-//        //Type2
+        //Type2
         else if(user3 == 2){
             init = init_date();
             System.out.println("Enter 1 for days , 2 for weeks");
@@ -73,20 +55,16 @@ public class Main {
             if(user4 ==1){
                 System.out.println("Enter number of days");
                 int days_input = scanner.nextInt() ;
-                days = days_input;
-                LocalDate day_diff1 = init.plusDays(days_input);
-
+                end = init.plusDays(days_input);
             }
+
             else if (user4 == 2){
                 System.out.println("Enter number of weeks");
                 int weeks_input = scanner.nextInt();
                 int final_days = weeks_input*7;
-                days = final_days;
-                LocalDate day_diff2 = init.plusDays(final_days);
+                end = init.plusDays(final_days);
             }
         }
-
-
 
         //Type3
         else {
@@ -96,76 +74,33 @@ public class Main {
             if(user5 == 1){
                 System.out.println("Enter number of days");
                 int days_input = scanner.nextInt() ;
-                LocalDate day_diff1 = end.minusDays(days_input);
+                init= end.minusDays(days_input);
             }
             else if (user5 ==2){
                 System.out.println("Enter number of weeks");
                 int weeks_input = scanner.nextInt();
                 int final_days = weeks_input*7;
-                LocalDate day_diff2 = end.minusDays(final_days);
+                init = end.minusDays(final_days);
 
             }
         }
+
+        Data start = new Data(area,init,end);
+        System.out.println(start.Quering_method(area,init,end));
+        System.out.println(start.get_cases());
+        System.out.println(start.get_deaths());
+        System.out.println(start.get_vaccinated());
+
+
+
 
 
 
         //Data reader:
-        String address = "covid-data.csv";
-        String line ;
-        ArrayList <Integer > cases = new ArrayList<>();
-        ArrayList <Integer > deaths = new ArrayList<>();
-        ArrayList <Long> vaccinated = new ArrayList<>();
-
-        try {
-            BufferedReader br = new BufferedReader(new FileReader(address));
-            DateTimeFormatter formatter = DateTimeFormatter.ofPattern("M/d/yyyy");
-            //skip first line
-            br.readLine();
-            while ((line = br.readLine()) != null) {
-                String[] values = line.split(",");
-                LocalDate compare = LocalDate.parse(values[3],formatter);
-                //since the user is going to input continent or country so i will define those into same variable
-                if (values[2].equals(area) | values[1].equals(area)) {
-                    // Convert value at column 4 to Integer;
-                    int case_num = Integer.parseInt(values[4]);
-                    if (compare.isAfter(init.minusDays(1)) && compare.isBefore(end.plusDays(1))) {
-
-                        //if the cells in csv file is blank
-                        if (values[5] == null || values[5].equals("")) {
-                            values[5] = String.valueOf(0);
-                        }
-                        if (values[6] == null || values[6].equals("")) {
-                            values[6] = String.valueOf(0);
-                        }
-                        int death_num = Integer.parseInt(values[5]);
-                        long vaccinated_num = Long.parseLong(values[6]);
-
-                        cases.add(case_num);
-                        deaths.add(death_num);
-                        vaccinated.add(vaccinated_num); }
-                }
-                else {
-                    br.readLine();
-                }
-            }
-            System.out.println(cases);
-            System.out.println(deaths);
-            System.out.println(vaccinated);
-        }
-        catch (IOException e) {
-            e.printStackTrace(); }
 
 
 
-        //case calculating
-//        int count= 0;
-//        List <Integer> nums =new ArrayList<>(cases);
-//        for (Integer num: nums  ){
-//            count += num;
-//        }
-//        System.out.println(count);
-//        Data data = new Data(area,days);
-//        data.Data_division();
+
 
 
 
