1064b6ca71e47c01edd6151b5f126eacae67b98f
Owen <s3891667@rmit.edu.vn>
2021-12-11 23:03:05 +0700

    done with the no grouping method

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,5 +1,4 @@
 package com.company;
-
 import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
@@ -7,10 +6,7 @@ import java.time.LocalDate;
 
 import java.time.format.DateTimeFormatter;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Scanner;
+import java.util.*;
 import java.io.IOException;
 
 import static java.time.temporal.ChronoUnit.DAYS;
@@ -18,7 +14,7 @@ import static java.time.temporal.ChronoUnit.DAYS;
 public class Main {
     public static void main(String[] args){
         int days = 0;
-        LocalDate init , end;
+        LocalDate init = null, end = null;
         Scanner scanner = new Scanner(System.in);
         System.out.println("Do you want to start the program (Y/N) ?");
 
@@ -69,8 +65,7 @@ public class Main {
             days = (int) DAYS.between(init, end);
         }
 
-
-        //Type2
+//        //Type2
         else if(user3 == 2){
             init = init_date();
             System.out.println("Enter 1 for days , 2 for weeks");
@@ -114,41 +109,70 @@ public class Main {
 
 
 
-
-
         //Data reader:
         String address = "covid-data.csv";
         String line ;
-        int count = 0;
+        ArrayList <Integer > cases = new ArrayList<>();
+        ArrayList <Integer > deaths = new ArrayList<>();
+        ArrayList <Long> vaccinated = new ArrayList<>();
+
         try {
-            BufferedReader br  = new BufferedReader(new FileReader(address));
-            while ((line = br.readLine()) !=null ){
-                String [] values = line.split(",");
-                List<String > al = Arrays.asList(values);
-                if (al.contains("Asia")){
-                    System.out.println(al.get(2));
-                    count +=1;
+            BufferedReader br = new BufferedReader(new FileReader(address));
+            DateTimeFormatter formatter = DateTimeFormatter.ofPattern("M/d/yyyy");
+            //skip first line
+            br.readLine();
+            while ((line = br.readLine()) != null) {
+                String[] values = line.split(",");
+                LocalDate compare = LocalDate.parse(values[3],formatter);
+                //since the user is going to input continent or country so i will define those into same variable
+                if (values[2].equals(area) | values[1].equals(area)) {
+                    // Convert value at column 4 to Integer;
+                    int case_num = Integer.parseInt(values[4]);
+                    if (compare.isAfter(init.minusDays(1)) && compare.isBefore(end.plusDays(1))) {
+
+                        //if the cells in csv file is blank
+                        if (values[5] == null || values[5].equals("")) {
+                            values[5] = String.valueOf(0);
+                        }
+                        if (values[6] == null || values[6].equals("")) {
+                            values[6] = String.valueOf(0);
+                        }
+                        int death_num = Integer.parseInt(values[5]);
+                        long vaccinated_num = Long.parseLong(values[6]);
+
+                        cases.add(case_num);
+                        deaths.add(death_num);
+                        vaccinated.add(vaccinated_num); }
+                }
+                else {
+                    br.readLine();
                 }
             }
-            System.out.println(count);
+            System.out.println(cases);
+            System.out.println(deaths);
+            System.out.println(vaccinated);
         }
         catch (IOException e) {
             e.printStackTrace(); }
 
 
-//
+
+        //case calculating
+//        int count= 0;
+//        List <Integer> nums =new ArrayList<>(cases);
+//        for (Integer num: nums  ){
+//            count += num;
+//        }
+//        System.out.println(count);
 //        Data data = new Data(area,days);
 //        data.Data_division();
 
+
+
+
+
+
     }
-
-
-
-
-
-
-
-
 
     public static LocalDate init_date(){
         String startDate1;
