f86c80e0e3ce90898ecd1ed1a17704e2e34df96c
GayeongLee21 <s3891958@rmit.edu.vn>
2021-12-30 11:23:16 +0900

    Done!

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -8,6 +8,7 @@ import java.util.*;
 public class Main {
     private static LocalDate startDate;
     private static LocalDate endDate;
+
     public static void main(String[] args)  {
         int num;        // data object type (e.g, Country = 1, Continent = 2)
         int rangeNum;   // time range type
@@ -19,8 +20,8 @@ public class Main {
         int resultType;
         String line;
         char user;
-        String area;
-
+        String area = null;
+        String[] array;
         List<List<String>> tmpList = new ArrayList<>();
 
         Scanner scanInt = new Scanner(System.in);
@@ -30,6 +31,8 @@ public class Main {
         List<List<String>> data = new ArrayList<>();
         File file = new File("covid-data.csv");
 
+        Interface interface_program = new Interface();
+
 
         try {       // get data from covid-data.csv
             FileReader fileName = new FileReader(file.getCanonicalPath());
@@ -37,7 +40,7 @@ public class Main {
 
             while ((line = br.readLine()) != null) {
 
-                String[] array = line.split(",");
+                array = line.split(",");
                 List<String> temp = Arrays.asList(array);
                 data.add(temp);
 
@@ -47,144 +50,200 @@ public class Main {
             e.printStackTrace();
         }
 
-        while (true) {
+
+        mainLoop: while (true) {
             startDate = null;   // initialize Date variable when loop begins
             endDate = null;
             tmpList.clear();
             groupDay = 0;
 
-            System.out.print("Do you want to start the program? (Y/N) ");
-
+            interface_program.Start_Stop_Program();
             user = scanMain.nextLine().charAt(0);
 
-            if (user == 'Y') {
-                System.out.println("|-----------------------------------------------|");
-                System.out.println("|          Welcome to Data management           |");
-                System.out.println("|-----------------------------------------------|");
-                System.out.println();
-                System.out.print("Please choose number 1 to input Continent, number 2 to input Country: ");
+            switch (user) {
+                case 'Y':
+                case 'y':
+                    System.out.println(" ____________________________________________________________________________________________________");
+                    System.out.println("|                                                                                                    |");
+                    System.out.println("|                                    Welcome to Data management                                      |");
+                    System.out.println("|                                                                                                    |");
+                    System.out.println("|____________________________________________________________________________________________________|");
 
-                num = scanInt.nextInt();
-                if (num == 1) {
-                    System.out.print("\nPlease input the Continent: ");
-                } else if (num == 2) {
-                    System.out.print("\nPlease input the Country: ");
-                }
-                area = scanString.nextLine();
-
-                System.out.println("-------------------------------------------------");
-                System.out.println("\nPlease choose the time range type");
-                System.out.println("Choose 1 for A pair of start date and end date(inclusive)");
-                System.out.println("Choose 2 for A number of days or weeks from a particular date");
-                System.out.println("Choose 3 for A number of days or weeks to a particular date");
-
-                rangeNum = scanInt.nextInt();
-
-                if (rangeNum == 1) {
-                    String start;
-                    String end;
-
-                    System.out.println("\nStart date: ");
-                    start = scanString.nextLine();
-
-                    System.out.println("End date: ");
-                    end = scanString.nextLine();
-
-                    startDate = changeFormat(start);
-                    endDate = changeFormat(end);
-
-                } else if (rangeNum == 2) {
-                    String start;
-
-                    System.out.println("\nEnter 1 for days, 2 for weeks: ");
-                    selectNum = scanInt.nextInt();
-
-                    if (selectNum == 1) {
-                        System.out.println("\nStart date: ");
-                        start = scanString.nextLine();
-
-                        System.out.println("A number of days: ");
-                        weekDays = scanInt.nextInt();
-
-                        startDate = changeFormat(start);
-                        endDate = changeFormat(start).plusDays(weekDays);
-
-                    } else if (selectNum == 2) {
-                        System.out.println("\nStart date: ");
-                        start = scanString.nextLine();
-
-                        System.out.println("A number of weeks: ");
-                        weekDays = scanInt.nextInt();
-
-                        startDate = changeFormat(start);
-                        endDate = changeFormat(start).plusWeeks(weekDays);
-
+                    interface_program.Intro_interface();
+                    num=interface_program.Interface_management("Continent","Country",null);
+                    if (num == 1) {
+                        area=interface_program.Enter_name("Continent");
+                    } else if (num == 2) {
+                        area=interface_program.Enter_name("Country");
                     }
-
-
-                } else if (rangeNum == 3) {
-                    String start;
-
-                    System.out.println("\nEnter 1 for days, 2 for weeks: ");
-                    selectNum = scanInt.nextInt();
-
-                    if (selectNum == 1) {
-                        System.out.println("\nStart date: ");
-                        start = scanString.nextLine();
-
-                        System.out.println("A number of days: ");
-                        weekDays = scanInt.nextInt();
-
-                        endDate = changeFormat(start);
-                        startDate = changeFormat(start).minusDays(weekDays);
-
-                    } else if (selectNum == 2) {
-                        System.out.println("\nStart date: ");
-                        start = scanString.nextLine();
-
-                        System.out.println("A number of weeks: ");
-                        weekDays = scanInt.nextInt();
-
-                        endDate = changeFormat(start);
-                        startDate = changeFormat(start).minusWeeks(weekDays);
-
-                    }
-                }
-
-
-                System.out.println("\nChoose 1 for no grouping, Choose 2 for divide data into number of groups, 3 for number of days: ");
-                groupType = scanInt.nextInt();
-
-                if ( groupType ==1){
                     System.out.println();
-                }
-                else if (groupType == 2) {
-                    System.out.println("Please input a number of groups: ");
-                    groupDay = scanInt.nextInt();
-                } else if (groupType == 3) {
-                    System.out.println("Please input a number of days: ");
-                    groupDay = scanInt.nextInt();
-                }
 
-                System.out.println("\nChoose 1 for positive cases, 2 for deaths, 3 for people vaccinated");
-                metricsNum = scanInt.nextInt() + 3;
+                    rangeNum=interface_program.Interface_management("A pair of start date and end date(inclusive)","A number of days or weeks from a particular date","A number of days or weeks to a particular date");
 
-                System.out.println("\nChoose 1 for New Total, 2 for Up To ");
-                resultType = scanInt.nextInt();
-                tmpList = dataProcess(data, area, num);
+                    switch (rangeNum) {
+                        case 1:
+                            String start;
+                            String end;
+
+                            interface_program.Interface_text("Start Date");
+                            start = scanString.nextLine();
+
+                            interface_program.Interface_text("End Date");
+                            end = scanString.nextLine();
+
+                            startDate = changeFormat(start);
+                            endDate = changeFormat(end);
+
+                            groupType=interface_program.Interface_management("NO GROUPING","DIVIDING DATA INTO A NUMBER OF GROUPS ","NUMBER OF DAYS");
+
+                            switch (groupType) {
+                                case 1:
+                                    System.out.println();
+                                    break;
+                                case 2:
+                                    interface_program.Interface_text("Please input a number of groups: ");
+                                    groupDay = scanInt.nextInt();
+                                    break;
+                                case 3:
+                                    interface_program.Interface_text("Please input a number of days: ");
+                                    groupDay = scanInt.nextInt();
+                                    break;
+                            }
+
+                            metricsNum=interface_program.Interface_management("positive cases","deaths","people vaccinated")+3;
+
+                            resultType=interface_program.Interface_management("New Total","Up To",null);
+
+                            tmpList = dataProcess(data, area, num);
+
+                            int displaytype = interface_program.Interface_management("Tabular","Chart",null);
+                            passing_value(groupType, groupDay, metricsNum, resultType, displaytype, tmpList);
+
+                            break;
 
 
-                System.out.println("\n Choose 1 for Tabular, 2 for Chart diplay");
-                int displaytype = scanInt.nextInt();
 
-                passing_value(groupType, groupDay, metricsNum, resultType, displaytype, tmpList);
+                        case 2:
+                            selectNum=interface_program.Interface_management("Days","Weeks",null);
+
+                            switch (selectNum) {
+                                case 1:
+                                    interface_program.Interface_text("Start Date");
+                                    start = scanString.nextLine();
+
+                                    interface_program.Interface_text("A number of days");
+                                    weekDays = scanInt.nextInt();
+
+                                    startDate = changeFormat(start);
+                                    endDate = changeFormat(start).plusDays(weekDays);
+                                    break;
+
+                                case 2:
+                                    interface_program.Interface_text("Start Date");
+                                    start = scanString.nextLine();
+
+                                    interface_program.Interface_text("A Number of Weeks");
+                                    weekDays = scanInt.nextInt();
+
+                                    startDate = changeFormat(start);
+                                    endDate = changeFormat(start).plusWeeks(weekDays);
+                                    break;
+                            }
+
+                            groupType=interface_program.Interface_management("NO GROUPING","DIVIDING DATA INTO A NUMBER OF GROUPS ","NUMBER OF DAYS");
+
+                            switch (groupType) {
+                                case 1:
+                                    System.out.println();
+                                    break;
+                                case 2:
+                                    interface_program.Interface_text("Please input a number of groups: ");
+                                    groupDay = scanInt.nextInt();
+                                    break;
+                                case 3:
+                                    interface_program.Interface_text("Please input a number of days: ");
+                                    groupDay = scanInt.nextInt();
+                                    break;
+                            }
+
+                            metricsNum=interface_program.Interface_management("positive cases","deaths","people vaccinated")+3;
+
+                            resultType=interface_program.Interface_management("New Total","Up To",null);
+
+                            tmpList = dataProcess(data, area, num);
+
+                            displaytype=interface_program.Interface_management("Tabular","Chart",null);
+                            passing_value(groupType, groupDay, metricsNum, resultType, displaytype, tmpList);
+
+                            break;
+
+
+                        case 3:
+                            selectNum=interface_program.Interface_management("Day","Week",null);
+
+                            switch (selectNum) {
+                                case 1:
+                                    interface_program.Interface_text("Start Date");
+                                    start = scanString.nextLine();
+
+                                    interface_program.Interface_text("A Number Of Days");
+                                    weekDays = scanInt.nextInt();
+
+                                    endDate = changeFormat(start);
+                                    startDate = changeFormat(start).minusDays(weekDays);
+                                    break;
+
+                                case 2:
+                                    interface_program.Interface_text("Start Date");
+                                    start = scanString.nextLine();
+
+                                    interface_program.Interface_text("A Number of Week");
+                                    weekDays = scanInt.nextInt();
+
+                                    endDate = changeFormat(start);
+                                    startDate = changeFormat(start).minusWeeks(weekDays);
+                                    break;
+                            }
+
+                            groupType=interface_program.Interface_management("NO GROUPING","DIVIDING DATA INTO A NUMBER OF GROUPS ","NUMBER OF DAYS");
+
+                            switch (groupType) {
+                                case 1:
+                                    System.out.println();
+                                    break;
+                                case 2:
+                                    interface_program.Interface_text("Please input a number of groups: ");
+                                    groupDay = scanInt.nextInt();
+                                    break;
+                                case 3:
+                                    interface_program.Interface_text("Please input a number of days: ");
+                                    groupDay = scanInt.nextInt();
+                                    break;
+                            }
+
+                            metricsNum=interface_program.Interface_management("positive cases","deaths","people vaccinated")+3;
+
+                            resultType=interface_program.Interface_management("New Total","Up To",null);
+
+                            tmpList = dataProcess(data, area, num);
+
+                            displaytype=interface_program.Interface_management("Tabular","Chart",null);
+                            passing_value(groupType, groupDay, metricsNum, resultType, displaytype, tmpList);
+                            break;
+
+
+                    }
+                    break;
+
+                case 'N':
+                case 'n':
+                    interface_program.end_program("Thank you for using our program!\n");
+                    break mainLoop;
+
+                default:
+                    interface_program.Interface_text("Please enter again");
 
             }
-
-            else if (user == 'N') {
-                System.out.println("\nThank you for using our program!\n");
-                break;
-            } else System.out.println("Please enter again");
 
         }
     }
@@ -201,7 +260,6 @@ public class Main {
     public static List<List<String>> dataProcess(List<List<String>> data, String continent, int num) {
         LocalDate date;
         List<List<String>> tempList = new ArrayList<>();
-
         if (num == 1) {
             for (int i = 1; i < data.size(); i++) {
                 date = changeFormat(data.get(i).get(3));
@@ -210,7 +268,6 @@ public class Main {
                     tempList.add(tmp);
                 }
             }
-
         } else if (num == 2) {
             for (int i = 1; i < data.size(); i++) {
                 date = changeFormat(data.get(i).get(3));
@@ -220,27 +277,11 @@ public class Main {
                 }
             }
         }
-
-        return tempList;
-    }
+        return tempList;}
 
     public static void passing_value(int groupType,int groupDay,int metricsNum,int resultType,int displaytype, List<List<String>> tmpList ){
         data_display data = new data_display(startDate,endDate,tmpList,groupType,groupDay,metricsNum,resultType,displaytype);
-        data.dataGroup(startDate, tmpList, groupType, groupDay, metricsNum, resultType);
-        data.displaying(displaytype, startDate, endDate);
+        data.dataGroup(tmpList, groupType, groupDay, metricsNum, resultType);
+
     }
-
-
 }
-
-
-
-}
-
-
-
-
-
-
-
-
